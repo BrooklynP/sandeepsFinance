f568f158cf90210cd9c42c8726dae6c7cc3aa8b0
BrooklynP <Brooklynpedley@gmail.com>
2021-04-24 14:19:35 +0100

    add iteration

--- index.js
+++ index.js
@@ -2,44 +2,99 @@ const http = require('http');
 const fs = require('fs');
 const Papa = require('papaparse');
 
-const file = fs.createWriteStream("data.txt");
+const dates = getDates(new Date('05-05-2020'), new Date());
 
-http.get('http://regsho.finra.org/CNMSshvol20210423.txt', (response) => {
-    // if(err) throw err;
+function freeze(time) {
+    const stop = new Date().getTime() + time;
+    while(new Date().getTime() < stop);       
+}
 
-    response.pipe(file)
+for (let i = 0; i < dates.length; i++) {
+    // console.log(dates[i])
+    let year = dates[i].getFullYear()
+    let month = dates[i].getMonth();
+    let date = dates[i].getDate();
+    if (month < 10) {
+        month = '0' + month
+    } else{
+        month = month.toString();
+    }
+    if (date < 10) {
+        date = '0' + date
+    }
+    else{
+        date = date.toString();
+    }
+    if(year = 20){
+        year = 2020
+    } else
+    if(year = 21){
+        year = 2021
+    }
+    else{
+        year = year.toString();
+    }
+    let fileName = year + month + date + '.txt';
+    console.log("Processing", fileName)
+    proccessData(fileName);
+    freeze(1000);
+}
+
+function getDates(startDate, stopDate) {
+    const dateArray = [];
+    const currentDate = new Date(startDate);
+    while (currentDate <= stopDate) {
+        dateArray.push(new Date(currentDate));
+        currentDate.setDate(currentDate.getDate() + 1);
+    }
+
+    return dateArray;
+}
 
 
-})
 
-setTimeout(() => {
-    fs.readFile('data.txt', 'utf8', (err, data) => {
-        if (err) { throw err; }
-        
-        let formattedData = Papa.parse(data, {
-            download: false,
-            delimiter: '|',
-            header: true
-        }).data;
+function proccessData(fileName) {
 
-        formattedData.length = formattedData.length - 2;
+    http.get('http://regsho.finra.org/CNMSshvol' + fileName, (response) => {
+        // if(err) throw err;
+        const file = fs.createWriteStream('data/' + fileName);
+        response.pipe(file)
+        console.log("piping to ", 'data/' + fileName)
 
-        for(let i=0; i < formattedData.length; i++){
-            console.log("Dealing with", formattedData[i].Symbol);
-            let csvData;
-            const fileCheck = fs.existsSync('tickers/' + formattedData[i].Symbol + '.csv');
-            if(fileCheck){
-                csvData = Papa.unparse([formattedData[i]], {header: false});
+
+    })
+
+    setTimeout(() => {
+        console.log("Gonna read file", fileName);
+        fs.readFile('data/' + fileName, 'utf8', (err, data) => {
+            if (err) { throw err; }
+
+            let formattedData = Papa.parse(data, {
+                download: false,
+                delimiter: '|',
+                header: true
+            }).data;
+
+            formattedData.length = formattedData.length - 2;
+
+            for (let i = 0; i < formattedData.length; i++) {
+                console.log("Dealing with", formattedData[i].Symbol);
+                let csvData;
+                const fileCheck = fs.existsSync('tickers/' + formattedData[i].Symbol + '.csv');
+                if (fileCheck) {
+                    csvData = Papa.unparse([formattedData[i]], { header: false });
+                }
+                else {
+                    csvData = Papa.unparse([formattedData[i]], { header: true });
+                }
+
+                console.log(csvData);
+                let safeSymbolName = formattedData[i].Symbol.replace('/', '-');
+                fs.appendFile('tickers/' + safeSymbolName + '.csv', csvData, 'utf8', response => { });
+
+
+
             }
-            else{
-                csvData = Papa.unparse([formattedData[i]], {header: true});
-            }
-
-            fs.appendFile('tickers/' + formattedData[i].Symbol + '.csv', csvData, response=>{});
-
-
-
-        }
-    }); 
-}, 2000);
-
+        });
+    }, 3600000);
+}
